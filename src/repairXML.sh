909df1213e72c184234936ae002dd1b527109d3b
Kyle Mills <kmills@mcs11.science.uoit.ca>
2014-10-11 10:29:17 -0400

    added comment that it requires xmllint

--- src/repairXML.sh
+++ src/repairXML.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 
+#requires xmllint program from the package  libxml2-utils:
+#  do 
+#      sudo apt-get install libxml2-utils
+#
+#  to get this package
+
+
 #A hackish way to fix the incomplete XML from early-terminated VASP runs.  More elegant solution required
 #This basically just closes all dangling XML tags
+
+
 
 for tag in `xmllint $1 2>&1 > /dev/null | grep 'Premature end of data in tag' | sed 's|.*Premature end of data in tag ||g' | sed 's| line [0-9]*||g'`; do
 
