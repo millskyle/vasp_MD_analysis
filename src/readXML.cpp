672d0d6ab00e60a558528eda7e1dedf410d8a06e
Kyle Mills <kyle.mills@uoit.net>
2015-07-19 18:29:13 -0400

    removed old commented TinyXML parsing code

--- src/readXML.cpp
+++ src/readXML.cpp
@@ -21,32 +21,6 @@ int str2int(string ss){
    return intValue;
 }
 
-void parse_atomtypes_tag(tag* atomtypesTag, FileInfo *vasprun ){
-/*   for (tag* level1 = atomtypesTag->FirstChildElement(); level1 != NULL; level1=level1->NextSiblingElement()) {
-      if (0==strcmp(level1->Value(),"set")) {
-         for (tag* rc = level1->FirstChildElement(); rc != NULL; rc = rc->NextSiblingElement()) {
-            atomType atomTypeData; //struct to hold the data from this line
-            int fieldcounter=0;
-            for (tag* c = rc->FirstChildElement(); c!=NULL; c = c->NextSiblingElement()) {
-               if (fieldcounter==0) {
-                  atomTypeData.atomspertype = str2int(c->FirstChild()->ToText()->Value());
-               } else if (fieldcounter==1) {
-                  atomTypeData.element = c->FirstChild()->ToText()->Value();
-               } else if (fieldcounter==2) {
-                  atomTypeData.mass = stod(c->FirstChild()->ToText()->Value());
-               } else if (fieldcounter==3) {
-                  atomTypeData.valence = stod(c->FirstChild()->ToText()->Value());
-               } else if (fieldcounter==4) {
-                  atomTypeData.pseudopotential = c->FirstChild()->ToText()->Value();
-               }
-               fieldcounter++; 
-               }
-               vasprun->atoms.push_back(atomTypeData);
-         }
-      }
-   }
-   */
-}
 
 bool update_3d_vector(vector<threevector>* objectToUpdate, float x, float y, float z) {
    threevector r;
@@ -55,14 +29,9 @@ bool update_3d_vector(vector<threevector>* objectToUpdate, float x, float y, flo
    return true;
 }
 
-//int readXML(FileInfo& info) {
 int readXML(FileInfo *vasprun) {
-   
-   /*redo this with pugixml cause it's way easier*/
-
    float x, y, z;
    vector<float> r;
-
 
    xml_document doc;
    screen.status << "Loading file into memory";
@@ -75,22 +44,8 @@ int readXML(FileInfo *vasprun) {
    }
    
   
-   
    screen.status << "Beginning to parse"   ;
 
-   int timestep_counter = 0;
-   
-  /* 
-   TiXmlDocument doc;
-//   if (doc.LoadFile(info.input_filename.c_str())) {
-   screen.status << "Loading file into memory" ;
-   if (!doc.LoadFile(vasprun->input_filename.c_str())) {
-      cerr << doc.ErrorDesc() << endl;
-      return 1;
-   }
-   screen.status << "File Loaded. Beginning to parse";
-   */
-   
    xml_node n_incar = doc.child("modeling").child("incar");
    key thisKey;
    for (xml_node i = n_incar.child("i"); i; i=i.next_sibling("i")) {
@@ -179,111 +134,6 @@ int readXML(FileInfo *vasprun) {
          vasprun->timesteps.push_back(thisStep);
       }
    }
-
-   //Get positions:
-//   xpath_node_set ns_positions = ns_timesteps.select_nodes(".//structure/varray[@name='positions']") ;
-
-
-//iterate over <array name="atoms"> tags (SHOULD ONLY BE ONE)
-/*   for (xpath_node_set::const_iterator it = atomsobject.begin(); it != atomsobject.end(); ++it) {
-      xpath_node node = *it;
-      cout << node.node().attribute("name").value() << endl;
-   }  */
-
-
-
-/* OLD PARSING UNNEEDED */
-/*   
-
-
-   for (tag* level1 = doc.FirstChildElement(); level1 != NULL; level1 = level1->NextSiblingElement()) {
-      if (0==strcmp(level1->Value(),"modeling")) {
-         for (tag* level2 = level1->FirstChildElement(); level2 != NULL; level2 = level2->NextSiblingElement()) {
-            *//*if (0==strcmp(level2->Value(),"incar")) {
-               for (tag* level3 = level2->FirstChildElement(); level3!=NULL; level3 = level3->NextSiblingElement()) {
-                  if (0==strcmp(level3->Value(), "i") && 0==strcmp(level3->Attribute("name"),"POTIM")) {
-                     vasprun->dt = stod(level3->FirstChild()->ToText()->Value());
-                  } else if (0==strcmp(level3->Value(), "i") && 0==strcmp(level3->Attribute("name"),"SYSTEM")) {
-                     vasprun->system_name = level3->FirstChild()->ToText()->Value();
-                  } else if (0==strcmp(level3->Value(), "i") && 0==strcmp(level3->Attribute("name"),"TEBEG")) {
-                     vasprun->starting_temperature = stod(level3->FirstChild()->ToText()->Value());
-                  }
-               }
-            } else *//* if (0==strcmp(level2->Value(),"atominfo")) {
-               for (tag0* level3 = level2->FirstChildElement(); level3 != NULL; level3 = level3->NextSiblingElement()) {
-                  if (0==strcmp(level3->Value(),"atoms")) {
-                     vasprun->numatoms = str2int(level3->FirstChild()->ToText()->Value());
-                  } else if (0==strcmp(level3->Value(),"types")) {
-                     vasprun->numtypes = str2int(level3->FirstChild()->ToText()->Value());
-                  } else if (0==strcmp(level3->Value(),"array")) {
-                     if (0==strcmp(level3->Attribute("name"), "atomtypes")) {
-                        parse_atomtypes_tag(level3, vasprun);
-                     }
-                  }
-               }
-            } else *//* if (0==strcmp(level2->Value(),"structure")){i
-               if (0==strcmp(level2->Attribute("name"),"initialpos")) {
-                  for (tag* level3 = level2->FirstChildElement(); level3 != NULL; level3 = level3->NextSiblingElement()) {
-                     if (0==strcmp(level3->Value(),"crystal")) {
-                        for (tag* level4 = level3->FirstChildElement(); level4 != NULL; level4 = level4->NextSiblingElement()) {
-                           if (0==strcmp(level4->Value(),"varray")) {
-                              if (0==strcmp(level4->Attribute("name"),"basis")) {
-                                 int dimension=1; //are we looking at x, y, or z vector?
-                                 for (tag* v = level4->FirstChildElement(); v!=NULL; v = v->NextSiblingElement()) {
-                                    float x,y,z;
-                                    sscanf(v->FirstChild()->ToText()->Value(), "  %f  %f  %f ", &x, &y, &z );
-                                    if (dimension==1){
-                                       update_3d_vector(&vasprun->latt, x,y,z);
-                                    } else if (dimension == 2) {
-                                       update_3d_vector(&vasprun->latt, x,y,z);
-                                    } else if (dimension ==3) {
-                                       update_3d_vector(&vasprun->latt, x,y,z);
-                                    }
-                                    dimension++;
-                                 }
-                              }
-                           }
-                        }
-                     }
-                  }              
-               }
-            } else *//* if (0==strcmp(level2->Value(),"calculation")) {
-               TimeStep thisStep;
-               for (tag* level3 = level2->FirstChildElement(); level3 != NULL; level3 = level3->NextSiblingElement()) {
-                  if (0==strcmp(level3->Value(),"structure")) {
-                     for (tag* level4 = level3->FirstChildElement("varray"); level4 != NULL; level4 = level4->NextSiblingElement()) {
-                        if (0==strcmp(level4->Attribute("name"),"positions")) {
-                           for (tag* vec = level4->FirstChildElement("v"); vec !=NULL; vec = vec->NextSiblingElement()) {
-                              float x,y,z;
-                              vector<float> r;
-                              sscanf(vec->FirstChild()->ToText()->Value(), "  %f  %f  %f ", &x, &y, &z );
-                              r.push_back(x*vasprun->latt[0][0]);
-                              r.push_back(y*vasprun->latt[1][1]);
-                              r.push_back(z*vasprun->latt[2][2]);
-                              thisStep.ppp.push_back(r);
-                           }
-                        } 
-                     }  
-                  } else if (0==strcmp(level3->Value(),"varray") && 0==strcmp(level3->Attribute("name"),"forces")) {
-                     for (tag* vec = level3->FirstChildElement("v"); vec !=NULL; vec = vec->NextSiblingElement()) {
-                        float x,y,z;
-                        vector<float> r;
-                        sscanf(vec->FirstChild()->ToText()->Value(), "  %f  %f  %f ", &x, &y, &z );
-                        r.push_back(x);
-                        r.push_back(y);
-                        r.push_back(z);
-                        thisStep.fff.push_back(r);
-                     }
-                  }
-               }
-               vasprun->timesteps.push_back(thisStep);
-               timestep_counter++;          
-            }
-         }
-      } 
-   }
-
-   */
 
    cout << "DONE PARSING" << endl;
 
