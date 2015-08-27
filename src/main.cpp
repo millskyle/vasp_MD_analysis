9daf537195e70be8d121787385a0f90fa4e222e1
Kyle Mills <kyle.mills@uoit.net>
2015-08-27 09:42:49 -0400

    removed old force field function, all calls to it.

--- src/main.cpp
+++ src/main.cpp
@@ -45,7 +45,6 @@ int main() {
       spatial_distribution_projection(&v, &config);
    }
    if (config.forces) {
-      force_field(&v, &config);
       force_bond_projections(&v, &config);
    }
 
